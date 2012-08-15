25883de593bee37c7aa06d44704368d15c88d292
Mark Muskardin <markmuskardin@Marks-MacBook-Pro-Maestro-Market.local>
2012-08-14 22:49:11 -0700

    more node.js working oh yeah

--- web.js
+++ web.js
@@ -1,96 +1,44 @@
-var express = require('express'),
-    jade = require('jade'),
-    path = require('path'),
-    mongoose = require('mongoose');
+var express = require('express')  
+  , mongoose = require('mongoose')
+  , todo = require('./models/todo')
+  , routes = require('./routes')
+  , sockets = require('./sockets')
+  , connect = require('express/node_modules/connect')
+  , RedisStore = require('connect-redis')(express)
+  , sessionStore = new RedisStore()
+  , app = express.createServer()
+  , sio;
 
-var app = express(express.logger());
-app.configure(function(){
-  app.set('views', path.join(__dirname, 'views'));
+app.configure(function () {
+  app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.set('view options');
-  app.locals.pretty = true;  
-  app.use("/", express.static(__dirname + '/public'));
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.cookieParser('keyboard cat'));
+  app.use(express.session({
+    secret: 'keyboard cat',
+    key: 'express.sid',
+    store: sessionStore
+  }));
   app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
 });
 
-mongoose.connect("mongodb://localhost/my_database");
-
-var Todo = mongoose.model("Todo", new mongoose.Schema({
-  text: String,
-  done: Boolean,
-  order: Number
- }));
-
-app.get('/', function(request,response){
-  response.render('layout', {
-      locals: {some: 'Locals'}
-  });
+app.configure('development', function () {
+  app.use(express.errorHandler());
+  app.locals.pretty = true;
 });
 
-app.get('/todo', function(req, res){
-  res.render('todo', {title: "Our sample application"});
-});
+routes.init(app);
+mongoose.connect("127.0.0.1", "todomvc", 27017);
 
-app.get('/api/todos', function(req, res){ return Todo.find(function(err, todos) {
-  return res.send(todos); });
-});
+app.listen(8888);
 
-app.get('/api/todos/:id', function(req, res){
-  return Todo.findById(req.params.id, function(err, todo) {
-    if (!err) {
-      return res.send(todo); }
-    }); 
-});
+sio = require('socket.io').listen(app);
+sockets.init(sio, sessionStore);
 
-app.put('/api/todos/:id', function(req, res){
-  return Todo.findById(req.params.id, function(err, todo) {
-    todo.text = req.body.text; 
-    todo.done = req.body.done; 
-    todo.order = req.body.order;
-    return todo.save(function(err) {
-      if (!err) { 
-        console.log("updated");
-      }
-      return res.send(todo); });
-    }); 
-});
+console.log("Express server listening on port 8888");
 
-app.post('/api/todos', function(req, res){ 
-  var todo;
-  todo = new Todo({
-     text: req.body.text,
-     done: req.body.done,
-     order: req.body.order
-  }); 
-  todo.save(function(err) {
-    if (!err) {
-      return console.log("created"); 
-    }
-  });
-  return res.send(todo); 
-});
-
-app.delete('/api/todos/:id', function(req, res){
-  return Todo.findById(req.params.id, function(err, todo) {
-    return todo.remove(function(err) { 
-      if (!err) {
-        console.log("removed");
-        return res.send('') 
-      }
-    }); 
-  });
-});
-
-
-var port = process.env.PORT || 8888;
-app.listen(port, function(){
-  console.log("listening on " + port);
-});
-
-// var redis = require('redis-url').connect(process.env.REDISTOGO_URL);
-// 
-// redist.set('foo','bar');
-// 
-// redis.get('foo', function(err,value){
-//   console.log('foo is: ' + value);
-// });
